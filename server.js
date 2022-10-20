f7afd01849d4f7cd4c3730aeedd88eb94e1573cf
Guru Prasad <57206722+kaliguru@users.noreply.github.com>
2022-10-20 20:31:16 +0530

    upadted ip address

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const app = require('./app');
 const port = process.env.PORT || 5000;
-const host = "localhost";
+const host = 139.59.44.34;
 
 const server = http.createServer(app);
 
