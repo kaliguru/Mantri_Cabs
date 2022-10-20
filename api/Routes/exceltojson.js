c16184304b2c3b7b82ae02f8ea7697cf7eb99648
Guru Prasad <57206722+kaliguru@users.noreply.github.com>
2022-10-20 20:33:55 +0530

    commented

--- api/Routes/exceltojson.js
+++ api/Routes/exceltojson.js
@@ -1,24 +1,24 @@
-const xlsx = require('xlsx');
-const fs =require('fs-extra');
-const express = require('express');
-const router = express.Router();
-const wb = xlsx.readFile('uploads/JUSTWIN.Xls')
-const ws = wb.Sheets['PRODUCTS']
-const chechAuth = require('../middleware/check-auth');
+// const xlsx = require('xlsx');
+// const fs =require('fs-extra');
+// const express = require('express');
+// const router = express.Router();
+// const wb = xlsx.readFile('uploads/JUSTWIN.Xls')
+// const ws = wb.Sheets['PRODUCTS']
+// const chechAuth = require('../middleware/check-auth');
 
 
 
-router.get('/',(req,res,next)=>
-{
-    console.log(wb.SheetNames)
-    console.log(ws)
-    const data = xlsx.utils.sheet_to_json(ws)
-    console.log(data);
-    res.status(200).json(data)
+// router.get('/',(req,res,next)=>
+// {
+//     console.log(wb.SheetNames)
+//     console.log(ws)
+//     const data = xlsx.utils.sheet_to_json(ws)
+//     console.log(data);
+//     res.status(200).json(data)
 
-})
+// })
 
-console.log(wb.SheetNames)
+// console.log(wb.SheetNames)
 
-module.exports = router
+// module.exports = router
 
